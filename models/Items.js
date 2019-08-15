eaf756e337ef3ac416dfe267e87c685c6385c8d8
Anna Trogimova <cobrana1000@gmail.com>
2019-08-15 15:23:36 +0200

    Items module done

---
+++ models/Items.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+
+const itemSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  img:String,
+  description:String,
+  year: Number,
+​  price: Number, 
+​  category:['clothing', 'shoes', 'accessories'],
+  isBought: {
+    type: Boolean,
+    default: false
+  }
+});
+
+const Item = mongoose.model('Item', itemSchema);
+
+module.exports = Item;
---
+++ routes/items.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/item', async (req, res, next) => {
+  
+})
+
+
+
+module.exports = router;
